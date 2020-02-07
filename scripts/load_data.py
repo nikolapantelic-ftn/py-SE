0ce37ebfa768cb87c3b773929f2f0bbfa29ae1bc
Nikola Pantelic <nikolapantelic434@gmail.com>
2020-02-07 22:23:03 +0100

    #parsiranje_skupa_HTML_dokumenata
    Cuvanje informacija o HTML dokumentima

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,21 +1,26 @@
 from modules.parser import Parser
 from modules.files import get_html_files
+from structs.html_info import HTMLInfo
 from structs.trie import Trie
 
 link_list = []
 word_list = []
+html_files = []
 trie = Trie()
 
 
 def parse_html(path):
-    global link_list, word_list, trie
+    global link_list, word_list, trie, html_files
     parser = Parser()
-    html_files = get_html_files(path)
-    for file in html_files:
+    html_paths = get_html_files(path)
+    for file in html_paths:
         print('Parsing file"' + file + '"')
         links, words = parser.parse(file)
-        link_list = link_list + links
-        word_list = word_list + words
+        html_files.append(HTMLInfo(file, links))
+        for word in words:
+            trie.add_word(word)
 
-    for word in word_list:
-        trie.add_word(word)
+    for file in html_files:
+        for check_file in html_files:
+            if file.path in check_file.links:
+                file.linked_in.append(check_file)
